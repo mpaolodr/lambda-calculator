d13ab7a7d578323a5aa9d69fbf7e834f86575b42
mpaolodr <mpaolodr_1504@yahoo.com>
2019-12-10 14:59:04 -0500

    prepared data in data.js for export and imported numbers array into numbers.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
+// example of import from data.js. Note all the ../   This is how we move through folders.
 /* 
 import { numbers } from '../../../data' 
 */
 //Import your array data to from the provided data file
+
+//NUMBERS DATA FROM data.js
+import { numbers } from "../../../data.js";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
